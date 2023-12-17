9b9e985656b4be81390dcd6a1ee18a70108f5135
Reiya Mitsuzono <reiya0914@gmail.com>
2023-12-17 21:37:43 +0900

    a

--- src/components/Assets/Buttons.js
+++ src/components/Assets/Buttons.js
@@ -1,84 +1,71 @@
-import Link from 'next/link';
-import styled from 'styled-components';
-import { SVG_calendar, SVG_guide_board, SVG_book } from '@/components/IconPack';
+import Link from "next/link";
+import styled from "styled-components";
+import { SVG_calendar, SVG_guide_board, SVG_book } from "@/components/IconPack";
 
 const Button = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 5px;
-`
+  width: 100%;
+  display: grid;
+  gap: 5px;
+`;
 
 const Frame = styled.div`
-    width: 100%;
-    height: 60px;
-    padding: 0 15px;
-    display: flex;
-    align-items: center;
-    gap: 10px;
-    border-radius: 10px;
-    overflow: hidden;
-`
+  height: 60px;
+  padding: 0 15px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  border-radius: 10px;
+  overflow: hidden;
+`;
 
 const TextBox = styled.div`
-    flex-direction: column;
-    justify-content: flex-end;
-    align-items: flex-start;
-    gap: 5px;
-    z-index:2;
-`
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+  gap: 5px;
+  z-index: 2;
+`;
 
 const Contexts = styled.div`
-    display: flex;
-    width: calc(100% - 20px);
-    align-items: flex-start;
-    gap: 15px;
-    overflow-x: scroll;
-    white-space: nowrap;
-    border-radius: 20px;
-    padding: 10px;
-`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: calc(100% - 20px);
+  gap: 15px;
+  overflow-x: scroll;
+  white-space: nowrap;
+  border-radius: 20px;
+  padding: 10px;
+`;
 
 export function Buttons(props) {
-    return (
-        <Button>
-            <div className='t-BlockTitle-Bolder tC-Text'>{props.title}</div>
-            <Contexts className='bC-BGSub e-Box'>
-                <Frame className='bC-BG e-Button'>
-                    <SVG_calendar size={30}/>
-                    <TextBox>
-                        <div className='t-Text-Bolder'>
-                            予定を見る
-                        </div>
-                        <div className='t-SubText-Normal tC-TextSub'>
-                            イベントカレンダー
-                        </div>
-                    </TextBox>
-                </Frame>
-                <Frame className='bC-BG e-Button'>
-                    <SVG_guide_board size={30}/>
-                    <TextBox>
-                        <div className='t-Text-Bolder'>
-                            交流する
-                        </div>
-                        <div className='t-SubText-Normal tC-TextSub'>
-                            コミュニティ
-                        </div>
-                    </TextBox>
-                </Frame>
-                <Frame className='bC-BG e-Button'>
-                    <SVG_book size={30}/>
-                    <TextBox>
-                        <div className='t-Text-Bolder'>
-                            過去を振りかえる
-                        </div>
-                        <div className='t-SubText-Normal tC-TextSub'>
-                            アーカイブ
-                        </div>
-                    </TextBox>
-                </Frame>
-            </Contexts>
-        </Button>
-    )
+  return (
+    <Button>
+      <div className="t-BlockTitle-Bolder tC-Text">{props.title}</div>
+      <Contexts className="bC-BGSub e-Box">
+        <Frame className="bC-BG e-Button">
+          <SVG_calendar size={30} />
+          <TextBox>
+            <div className="t-Text-Bolder">予定を見る</div>
+            <div className="t-SubText-Normal tC-TextSub">
+              イベントカレンダー
+            </div>
+          </TextBox>
+        </Frame>
+        <Frame className="bC-BG e-Button">
+          <SVG_guide_board size={30} />
+          <TextBox>
+            <div className="t-Text-Bolder">交流する</div>
+            <div className="t-SubText-Normal tC-TextSub">コミュニティ</div>
+          </TextBox>
+        </Frame>
+        <Frame className="bC-BG e-Button">
+          <SVG_book size={30} />
+          <TextBox>
+            <div className="t-Text-Bolder">過去を振りかえる</div>
+            <div className="t-SubText-Normal tC-TextSub">アーカイブ</div>
+          </TextBox>
+        </Frame>
+      </Contexts>
+    </Button>
+  );
 }
--- src/components/Assets/Buttons2.js
+++ src/components/Assets/Buttons2.js
@@ -1,41 +1,91 @@
 import Link from "next/link";
+import Image from "next/image";
 import styled from "styled-components";
 import { SVG_calendar, SVG_guide_board, SVG_book } from "@/components/IconPack";
 
 const Button = styled.div`
   width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+  display: grid;
   gap: 5px;
 `;
 
 const Frame = styled.div`
-  width: 100%;
-  height: 60px;
-  padding: 0 15px;
-  display: flex;
+  height: 90px;
+  padding: 5px 5px 5px 10px;
+  display: grid;
+  grid-template-columns: max-content 1fr;
   align-items: center;
-  gap: 10px;
+  gap: 5px;
   border-radius: 10px;
   overflow: hidden;
 `;
+// const Frame = styled.div`
+//   height: 90px;
+//   padding: 5px 5px 5px 10px;
+//   display: flex;
+//   align-items: center;
+//   gap: 5px;
+//   border-radius: 10px;
+//   overflow: hidden;
+// `;
+
+const GroupArtwork = styled.div`
+  position: relative;
+  height: 75px;
+
+  * {
+    border-radius: 30px;
+    position: absolute;
+    overflow: hidden;
+  }
+
+  & :nth-child(1) {
+    top: 0;
+    left: 0;
+    opacity: 0.5;
+  }
+
+  & :nth-child(2) {
+    top: 15px;
+    left: 15px;
+  }
+`;
+
+const UserArtwork = styled.div`
+  position: relative;
+  height: 75px;
+
+  * {
+    border-radius: 30px;
+    position: absolute;
+    overflow: hidden;
+    top: 7.5px;
+    left: 7.5px;
+  }
+`;
 
 const TextBox = styled.div`
+  width: auto;
+  display: flex;
   flex-direction: column;
   justify-content: flex-end;
   align-items: flex-start;
-  gap: 5px;
-  z-index: 2;
+  gap: 2px;
+
+  * {
+    /* text-wrap: wrap; */
+    white-space: normal;
+    word-wrap: break-word;
+    overflow-wrap: break-word;
+  }
 `;
 
 const Contexts = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
   width: calc(100% - 20px);
-  align-items: flex-start;
   gap: 15px;
   overflow-x: scroll;
-  white-space: nowrap;
   border-radius: 20px;
   padding: 10px;
 `;
@@ -46,26 +96,89 @@ export function Buttons2(props) {
       <div className="t-BlockTitle-Bolder tC-Text">{props.title}</div>
       <Contexts className="bC-BGSub e-Box">
         <Frame className="bC-BG e-Button">
-          <SVG_calendar size={30} />
+          <div style={{width: '75px', height: '75px'}}>
+            <GroupArtwork>
+              <Image
+                src="/DummyAssets/Square/rusk_1024.png"
+                width={45}
+                height={45}
+                alt='Artwork'
+              />
+              <Image className="e-Button"
+                src="/DummyAssets/Square/ama_1024.png"
+                width={60}
+                height={60}
+                alt='Artwork'
+              />
+            </GroupArtwork>
+          </div>
           <TextBox>
-            <div className="t-Text-Bolder">予定を見る</div>
-            <div className="t-SubText-Normal tC-TextSub">
-              イベントカレンダー
-            </div>
+            <div className="t-Text-Bolder">Group Name</div>
+            <div className="t-SubText-Normal tC-TextSub">Theme</div>
+            <div className="t-SubText-Normal tC-TextSub">Num of Followers</div>
           </TextBox>
         </Frame>
         <Frame className="bC-BG e-Button">
-          <SVG_guide_board size={30} />
+          <div style={{width: '75px', height: '75px'}}>
+            <UserArtwork>
+              <Image className="e-Button"
+                src="/DummyAssets/Square/ama_1024.png"
+                width={60}
+                height={60}
+                alt='Artwork'
+              />
+            </UserArtwork>
+          </div>
           <TextBox>
-            <div className="t-Text-Bolder">交流する</div>
-            <div className="t-SubText-Normal tC-TextSub">コミュニティ</div>
+            <div className="t-Text-Bolder">Group Name</div>
+            <div className="t-SubText-Normal tC-TextSub">Theme</div>
+            <div className="t-SubText-Normal tC-TextSub">Num of Followers</div>
           </TextBox>
         </Frame>
         <Frame className="bC-BG e-Button">
-          <SVG_book size={30} />
+          <div style={{width: '75px', height: '75px'}}>
+            <GroupArtwork>
+              <Image
+                src="/DummyAssets/Square/rusk_1024.png"
+                width={45}
+                height={45}
+                alt='Artwork'
+              />
+              <Image className="e-Button"
+                src="/DummyAssets/Square/ama_1024.png"
+                width={60}
+                height={60}
+                alt='Artwork'
+              />
+            </GroupArtwork>
+          </div>
           <TextBox>
-            <div className="t-Text-Bolder">過去を振りかえる</div>
-            <div className="t-SubText-Normal tC-TextSub">アーカイブ</div>
+            <div className="t-Text-Bolder">Looooooooooooooooooooooong Group Name</div>
+            <div className="t-SubText-Normal tC-TextSub">Theme</div>
+            <div className="t-SubText-Normal tC-TextSub">Num of Followers</div>
+          </TextBox>
+        </Frame>
+        <Frame className="bC-BG e-Button">
+          <div style={{width: '75px', height: '75px'}}>
+            <GroupArtwork>
+              <Image
+                src="/DummyAssets/Square/rusk_1024.png"
+                width={45}
+                height={45}
+                alt='Artwork'
+              />
+              <Image className="e-Button"
+                src="/DummyAssets/Square/ama_1024.png"
+                width={60}
+                height={60}
+                alt='Artwork'
+              />
+            </GroupArtwork>
+          </div>
+          <TextBox>
+            <div className="t-Text-Bolder">Group Name</div>
+            <div className="t-SubText-Normal tC-TextSub">Theme</div>
+            <div className="t-SubText-Normal tC-TextSub">Num of Followers</div>
           </TextBox>
         </Frame>
       </Contexts>
