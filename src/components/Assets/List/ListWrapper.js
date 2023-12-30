bf3f9eb1955cfde0b0675698abd50d9513caae30
Reiya Mitsuzono <reiya0914@gmail.com>
2023-12-30 10:57:43 +0900

    a

--- src/components/Assets/List/ListWrapper.js
+++ src/components/Assets/List/ListWrapper.js
@@ -6,6 +6,12 @@ const Button = styled.div`
   gap: 5px;
 `;
 
+const TagWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 5px;
+`
+
 const Contexts = styled.div`
   display: grid;
   width: calc(100% - 20px);
@@ -27,6 +33,7 @@ export function ListWrapper(props) {
   return (
     <Button>
       <div className="t-BlockTitle-Bolder tC-Text">{props.title}</div>
+      <TagWrapper></TagWrapper>
       <Contexts className="bC-BGSub e-Box" style={{gridTemplateColumns: `repeat(${props.column}, 1fr)`, gap: `${gap}`, borderRadius: `${borderRadius}`}}>
         {props.Contexts}
       </Contexts>
